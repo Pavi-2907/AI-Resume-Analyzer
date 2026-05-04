d016cab2585f77f4b4f01b426ef0bad76b6679fc
Pavithra K <pavithra.kk29@gmail.com>
2026-05-04 14:25:23 +0530

    Add files via upload

---
+++ skills.py
@@ -0,0 +1,29 @@
+TECHNICAL_SKILLS = [
+    # Programming
+    "python", "java", "c", "c++", "c#", "javascript", "typescript",
+    "go", "rust", "r", "matlab",
+
+    # Web
+    "html", "css", "react", "angular", "vue",
+    "node", "express", "flask", "django", "spring", "bootstrap",
+
+    # Data & ML
+    "machine learning", "deep learning", "nlp",
+    "tensorflow", "keras", "pytorch", "scikit-learn",
+    "pandas", "numpy", "opencv", "xgboost",
+
+    # Database
+    "sql", "mysql", "postgresql", "mongodb", "oracle",
+
+    # Cloud & DevOps
+    "aws", "azure", "gcp", "docker", "kubernetes",
+    "jenkins", "git", "github", "gitlab",
+
+    # Tools
+    "power bi", "tableau", "excel", "plotly",
+    "streamlit", "statsmodels",
+
+    # Concepts
+    "cnn", "rnn", "transformers",
+    "computer vision", "data analysis", "data science"
+]
